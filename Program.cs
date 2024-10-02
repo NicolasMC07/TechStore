60ecaa78525e2bbc95d5faf0dded10b263c761a3
NicolasMC07 <nicolasmarcar2006@gmail.com>
2024-10-02 09:00:54 -0500

    feat: adding database credentials

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+Env.Load();
+
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var conectionDB = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(conectionDB, ServerVersion.Parse("8.0.20-mysql")));
+
+
+// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
